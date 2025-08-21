28a1f235b5740a1294deb1fda9dc5652160d1a2e
TelmoPersch <119141423+telmopersch123@users.noreply.github.com>
2025-08-21 19:32:44 -0400

    alterações no build

--- replace-env.js
+++ replace-env.js
@@ -4,13 +4,13 @@ const path = require("path");
 const envFile = path.join(__dirname, "src/environments/environment.prod.ts");
 
 // Pega a variável da Vercel
-const apiUrl = process.env.API_URL || "http://localhost:3000/api";
+const apiUrl = process.env.apiUrl || "http://localhost:3000/api";
 
 const content = `export const environment = {
   production: true,
-  API_URL: '${apiUrl}'
+  apiUrl: '${apiUrl}'
 };
 `;
 
 fs.writeFileSync(envFile, content, { encoding: "utf8" });
-console.log("environment.prod.ts atualizado com API_URL:", apiUrl);
+console.log("environment.prod.ts atualizado com apiUrl:", apiUrl);
